6e9fef1df3bcb42d89bc865757a3d8af65826b4d
Liu Chaoyang <chaoyanglius@gmail.com>
2018-03-24 21:15:58 +0800

    Add license into this project
    * add license into codes
    * add license file
    * replace "new" by "malloc"

--- cpu_medianfilter_1D.c
+++ cpu_medianfilter_1D.c
@@ -1,3 +1,25 @@
+/**
+ * Author: Liu Chaoyang
+ * E-mail: chaoyanglius@gmail.com
+ * 
+ * Median filter using C
+ * Copyright (C) 2018 Liu Chaoyang
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <memory.h>
@@ -60,7 +82,7 @@ void medianfilter(element* signal, element* result, int length)
 		return;
 	}
 	//   Allocate memory for signal extension
-	element* extension = new element[length + 2 * radius];
+	element* extension = (element*)malloc((length + 2 * radius) * sizeof(element));
 	//   Check memory allocation
 	if (!extension)
 		return;
@@ -74,7 +96,7 @@ void medianfilter(element* signal, element* result, int length)
 	//   Call median filter implementation
 	_medianfilter(extension, result ? result : signal, length + 2 * radius);
 	//   Free memory
-	delete[] extension;
+   free(extension);
 }
 
 int main(int argc, char **argv)
